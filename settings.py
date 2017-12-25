0857b2cc8dd88e2829564da7db67d8205090764c
Al Johri <al.johri@gmail.com>
2017-12-25 09:50:44 -0500

    update notebooks

---
+++ settings.py
@@ -0,0 +1,15 @@
+ONE_HOUR = 3600
+HALF_HOUR = ONE_HOUR / 2
+TEN_MINUTES = ONE_HOUR / 6
+
+MIN_BEDS = 3
+MIN_BATHS = 2
+MIN_PRICE = 300000
+MAX_PRICE = 525000
+MAX_TAX = 16000
+
+blacklist_cities = [
+    'Paterson', 'East Orange', 'City of Orange', 'Belleville', 
+    'Elizabeth', 'Jefferson', 'Passaic', 'Newark', 'East Newark',
+    'Linden', 'Nutley', 'Garfield', 'Hillside', 'Little Ferry',
+    'Perth Amboy', 'Irvington', 'Carteret', 'Rahway',]
